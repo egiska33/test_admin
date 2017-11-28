d30e869d3745e5c43dbfa0b59dfa54cc47b98558
egiska33 <esivickas@gmail.com>
2017-11-28 10:32:28 +0200

    Add employees table and reliationship

--- database/migrations/2017_11_28_081525_create_companies_table.php
+++ database/migrations/2017_11_28_081525_create_companies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateEmployeesTable extends Migration
+class CreateCompaniesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateEmployeesTable extends Migration
      */
     public function up()
     {
-        Schema::create('employees', function (Blueprint $table) {
+        Schema::create('companies', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('company_id')->unsigned();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('address');
+            $table->string('logo');
             $table->timestamps();
-            $table->foreign('company_id')->references('id')->on('companies');
         });
     }
 
@@ -30,6 +29,6 @@ class CreateEmployeesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('employees');
+        Schema::dropIfExists('companies');
     }
 }
